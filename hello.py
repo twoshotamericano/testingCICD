75d851da2ad674e223a9b548c2b6643ab8a45c7b
Edward Anderson <edward.anderson@probitas1492.com>
2022-08-11 17:47:05 +0100

    updated linting

--- hello.py
+++ hello.py
@@ -4,6 +4,12 @@ import click
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
+    """Hello Function
+
+    Args:
+        name (_type_): _description_
+        color (_type_): _description_
+    """
     if name == "Thor":
         click.echo("Thor, you are always red.")
         click.echo(click.style(f"Hello {name}!", fg="red"))
@@ -11,5 +17,7 @@ def hello(name, color):
         click.echo(f"Your color is {color}!")
         click.echo(click.style(f"Hello {name}!", fg=color))
 
+A="1233"
+
 if __name__ == "__main__":
-    hello()
+    hello("Edward","Blue")
